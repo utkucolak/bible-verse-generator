8886c1304da27d71f8111f7cb8514d46b6053f29
utkucolak <utkucolak05@gmail.com>
2020-09-02 20:06:47 +0300

    data adding

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bible-verse-generator
+
+A simple NLP model to generate new bible verses. Test commit
---
+++ model.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+import numpy as np 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, GlobalAveragePooling1D
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
